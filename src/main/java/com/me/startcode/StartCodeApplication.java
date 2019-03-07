3642ab251204b0209bef88f3a1b97b600df98d2a
wk <ymxk666@163.com>
2019-03-07 15:41:09 +0800

    init

---
+++ src/main/java/com/me/startcode/StartCodeApplication.java
@@ -0,0 +1,13 @@
+package com.me.startcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/me/startcode/StartCodeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.me.startcode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StartCodeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
